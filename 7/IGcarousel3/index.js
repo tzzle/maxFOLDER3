f00c5c6b0bc5e24cdf0f8bc26be409e325f8154c
tzzle <130739462+tzzle@users.noreply.github.com>
2023-06-04 15:05:17 +0300

    Add files via upload

---
+++ 7/IGcarousel3/index.js
@@ -0,0 +1,8 @@
+/*JAVASCRIPT*/
+function color1() {
+    document.getElementById("bckgrndClr").style.backgroundColor = "#116D6E";
+}
+
+function color2() {
+    document.getElementById("bckgrndClr").style.backgroundColor = "#F2D8D8";
+}
---
+++ 8/IGcarousel45/index.js
@@ -0,0 +1,10 @@
+//JAVASCRIPT
+function bulbOn() {
+    document.getElementById("bulb").src = 
+    'images/pic_bulbon.gif';
+}
+
+function bulbOff() {
+    document.getElementById("bulb").src = 
+    'images/pic_bulboff.gif';
+}
